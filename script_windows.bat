99abcbeeb4eb86535afd51e425bdd02f334abbce
Alberto Bettini <alberto.bettini@marconirovereto.it>
2024-07-19 10:16:50 +0200

    aggiunta file per esecuzione e creazione TestRunner

---
+++ script_windows.bat
@@ -0,0 +1,31 @@
+@echo off
+rem Script per compilare ed eseguire il progetto ListAdapter
+
+rem Compilazione del codice sorgente
+echo Compilazione del codice sorgente...
+javac -cp ".;cldc-1.1.jar;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" myAdapter/*.java myTest/*.java
+
+rem Verifica se la compilazione è stata completata con successo
+if %errorlevel% equ 0 (
+    echo Compilazione completata con successo.
+
+    rem Esecuzione dei test
+    echo Esecuzione dei test...
+    java -cp ".;cldc-1.1.jar;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" myTest.TestRunner
+
+    rem Chiedi all'utente se desidera eliminare tutti i file .class
+    set /p risposta="Vuoi eliminare tutti i file .class dopo l'esecuzione? (si/no) "
+
+    rem Controlla la risposta dell'utente
+    if "%risposta%"=="si" (
+        rem Se l'utente ha risposto si, elimina tutti i file .class
+        echo Eliminazione di tutti i file .class...
+        for /r %%f in (*.class) do del /F /Q "%%f"
+        echo Tutti i file .class sono stati eliminati.
+    ) else (
+        rem Se l'utente ha risposto no, non fare nulla
+        echo Non vengono eliminati i file .class.
+    )
+) else (
+    echo Errore durante la compilazione del codice sorgente. Impossibile eseguire i test.
+)
